d95e810691e32c008245ab0a41e7f201769f5e4a
Aris Karamat <karamataris@gmail.com>
2025-05-13 15:36:21 +0200

    above the fold style completed

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-main',
-  imports: [AboveTheFoldComponent],
+  imports: [AboveTheFoldComponent, AboutMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
