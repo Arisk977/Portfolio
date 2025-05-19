03d06debb5dc21daefc1690f65ce573554e3b5ed
Aris Karamat <karamataris@gmail.com>
2025-05-19 16:00:22 +0200

    feedback style improved

--- src/app/colleagues-thoughts/colleagues-thoughts.component.ts
+++ src/app/colleagues-thoughts/colleagues-thoughts.component.ts
@@ -14,19 +14,19 @@ export class ColleaguesThoughtsComponent {
     title: 'Frontend Developer',
     p: `Karl really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.`,
     img: './../../../assets/feedback-card-1.png'
-};
+  };
 
-feedback2: Feedback = {
+  feedback2: Feedback = {
     name: 'Maria Schäfer',
     title: 'Frontend Developer',
     p: `It was a great pleasure to work with Karl. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.`,
     img: './../../../assets/feedback-card-2.png'
-};
+  };
 
- feedback3: Feedback = {
+  feedback3: Feedback = {
     name: 'David Braun',
     title: 'Frontend Developer',
     p: `Karl was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.`,
     img: './../../../assets/feedback-card-1.png'
-};
+  };
 }
