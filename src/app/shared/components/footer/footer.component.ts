462fa3d63f3a5795964d57c6f1cf8f20ece058d7
Aris Karamat <karamataris@gmail.com>
2025-05-22 16:57:30 +0200

    added svg text data

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SvgComponent } from './svg/svg.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, SvgComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
