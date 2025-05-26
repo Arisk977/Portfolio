50f44336da98f3cdcf8da19e9d2362b369adb327
Aris Karamat <karamataris@gmail.com>
2025-05-26 18:35:10 +0200

    footer-line animation

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SvgComponent } from './svg/svg.component';
 
 @Component({
@@ -8,6 +8,33 @@ import { SvgComponent } from './svg/svg.component';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  private intervalId: any;
 
+ngOnInit(): void {
+  const footerLine = document.querySelector('.footer-line');
+
+  if (footerLine) {
+    let showing = true;
+
+    this.intervalId = setInterval(() => {
+      if (showing) {
+        footerLine.classList.remove('show');
+        footerLine.classList.add('hide');
+      } else {
+        footerLine.classList.remove('hide');
+        footerLine.classList.add('show');
+      }
+
+      showing = !showing;
+    }, 3000);
+  }
+}
+
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
