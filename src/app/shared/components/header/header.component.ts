9b63c4be551006d26728736da11eb5a083f9edaa
Aris Karamat <karamataris@gmail.com>
2025-06-16 17:47:56 +0200

    header sticky

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Router } from '@angular/router';
 import { LanguageService } from '../../../services/language.service';
@@ -12,10 +12,10 @@ import { LanguageService } from '../../../services/language.service';
 })
 export class HeaderComponent implements OnInit {
   isGerman: boolean = false;
+  lastScrollTop = 0;
+  headerVisible = true;
 
-  constructor(public router: Router, private langService: LanguageService) {
-
-  }
+  constructor(public router: Router, private langService: LanguageService) {}
 
   ngOnInit(): void {
     this.langService.isGerman$.subscribe(value => {
@@ -32,5 +32,16 @@ export class HeaderComponent implements OnInit {
 
   isSingleProjectPage(): boolean {
     return this.router.url.includes('/project');
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > this.lastScrollTop && currentScroll > 100) {
+      this.headerVisible = false;
+    } else {
+      this.headerVisible = true;
+    }
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
   }
 }
